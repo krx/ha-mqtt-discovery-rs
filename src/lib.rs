8a5ed467c1815b6bf510a8eadfc6aeb2c2ef0b9b
Kareem ElFaramawi <kareem02@gmail.com>
2024-10-15 23:11:18 -0400

    remove existing mqtt lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
 #![recursion_limit = "256"]
 
-use anyhow::{anyhow, Result};
 use mqtt::{
     alarm_control_panel::AlarmControlPanel, binary_sensor::BinarySensor, button::Button,
     camera::Camera, climate::Climate, cover::Cover, device_tracker::DeviceTracker,
@@ -9,92 +8,87 @@ use mqtt::{
     sensor::Sensor, siren::Siren, switch::Switch, tag::Tag, text::Text, update::Update,
     vacuum::Vacuum, valve::Valve, water_heater::WaterHeater,
 };
-use rumqttc::v5::{
-    mqttbytes::{v5::PublishProperties, QoS::AtLeastOnce},
-    AsyncClient,
-};
-use serde::Serialize;
+//use serde::Serialize;
 
-pub use rumqttc::v5;
-use serde_json::Value;
+use serde_json::{Value, Error};
 
 pub mod mqtt;
 
-const ONE_WEEK_SECONDS: u32 = 60 * 60 * 24 * 7;
+//const ONE_WEEK_SECONDS: u32 = 60 * 60 * 24 * 7;
 
-#[derive(Clone)]
-pub struct HomeAssistantMqtt {
-    client: AsyncClient,
-    discovery_prefix: String,
-}
+//#[derive(Clone)]
+//pub struct HomeAssistantMqtt {
+//    client: AsyncClient,
+//    discovery_prefix: String,
+//}
 
-impl HomeAssistantMqtt {
-    pub fn new<S: Into<String>>(client: AsyncClient, discovery_prefix: S) -> Self {
-        Self {
-            client,
-            discovery_prefix: discovery_prefix.into(),
-        }
-    }
-
-    /// The discovery topic needs to follow a specific format:
-    /// `<discovery_prefix>/<component>/[<node_id>/]<object_id>/config`
-    ///
-    /// - `<discovery_prefix>`: The Discovery Prefix defaults to homeassistant. This prefix can be changed.
-    /// - `<component>`: One of the supported MQTT integrations, eg. binary_sensor.
-    /// - `<node_id>` (Optional): ID of the node providing the topic, this is not used by Home Assistant but may be used to structure the MQTT topic. The ID of the node must only consist of characters from the character class [a-zA-Z0-9_-] (alphanumerics, underscore and hyphen).
-    /// - `<object_id>`: The ID of the device. This is only to allow for separate topics for each device and is not used for the entity_id. The ID of the device must only consist of characters from the character class [a-zA-Z0-9_-] (alphanumerics, underscore and hyphen).
-    ///
-    /// The `<node_id>` level can be used by clients to only subscribe to their own (command) topics by using one wildcard topic like <discovery_prefix>/+/<node_id>/+/set.
-    ///
-    /// Best practice for entities with a unique_id is to set `<object_id>` to unique_id and omit the `<node_id>`.
-    pub async fn publish_entity(&self, entity: Entity) -> Result<()> {
-        let component = entity.get_component_name();
-        let attributes = entity.get_attributes()?;
-        let object_id = attributes
-            .as_object()
-            .ok_or(anyhow!("entity configuration should be an object"))?
-            .get("uniq_id")
-            .ok_or(anyhow!(
-                "entity configuration should have an attribute 'uniq_id'"
-            ))?
-            .as_str()
-            .ok_or(anyhow!("'uniq_id' attribute should be a string"))?;
-        let prefix = self
-            .discovery_prefix
-            .strip_suffix("/")
-            .unwrap_or(&self.discovery_prefix);
-        let topic = format!("{prefix}/{component}/{object_id}/config");
-        let payload = serde_json::ser::to_string(&attributes).unwrap();
-        let props = PublishProperties {
-            //payload_format_indicator: Some(1),
-            message_expiry_interval: Some(ONE_WEEK_SECONDS),
-            content_type: Some("application/json".to_string()),
-            ..Default::default()
-        };
-        Ok(self
-            .client
-            .publish_with_properties(topic, AtLeastOnce, true, payload, props)
-            .await?)
-    }
-
-    pub async fn publish_data<S: Serialize>(
-        &self,
-        topic: &String,
-        payload: &S,
-        message_expiry_interval: Option<u32>,
-    ) -> Result<()> {
-        let payload = serde_json::ser::to_string(payload).unwrap();
-        let props = PublishProperties {
-            message_expiry_interval,
-            content_type: Some("application/json".to_string()),
-            ..Default::default()
-        };
-        Ok(self
-            .client
-            .publish_with_properties(topic, AtLeastOnce, true, payload, props)
-            .await?)
-    }
-}
+//impl HomeAssistantMqtt {
+//    pub fn new<S: Into<String>>(client: AsyncClient, discovery_prefix: S) -> Self {
+//        Self {
+//            client,
+//            discovery_prefix: discovery_prefix.into(),
+//        }
+//    }
+//
+//    /// The discovery topic needs to follow a specific format:
+//    /// `<discovery_prefix>/<component>/[<node_id>/]<object_id>/config`
+//    ///
+//    /// - `<discovery_prefix>`: The Discovery Prefix defaults to homeassistant. This prefix can be changed.
+//    /// - `<component>`: One of the supported MQTT integrations, eg. binary_sensor.
+//    /// - `<node_id>` (Optional): ID of the node providing the topic, this is not used by Home Assistant but may be used to structure the MQTT topic. The ID of the node must only consist of characters from the character class [a-zA-Z0-9_-] (alphanumerics, underscore and hyphen).
+//    /// - `<object_id>`: The ID of the device. This is only to allow for separate topics for each device and is not used for the entity_id. The ID of the device must only consist of characters from the character class [a-zA-Z0-9_-] (alphanumerics, underscore and hyphen).
+//    ///
+//    /// The `<node_id>` level can be used by clients to only subscribe to their own (command) topics by using one wildcard topic like <discovery_prefix>/+/<node_id>/+/set.
+//    ///
+//    /// Best practice for entities with a unique_id is to set `<object_id>` to unique_id and omit the `<node_id>`.
+//    pub async fn publish_entity(&self, entity: Entity) -> Result<()> {
+//        let component = entity.get_component_name();
+//        let attributes = entity.get_attributes()?;
+//        let object_id = attributes
+//            .as_object()
+//            .ok_or(anyhow!("entity configuration should be an object"))?
+//            .get("uniq_id")
+//            .ok_or(anyhow!(
+//                "entity configuration should have an attribute 'uniq_id'"
+//            ))?
+//            .as_str()
+//            .ok_or(anyhow!("'uniq_id' attribute should be a string"))?;
+//        let prefix = self
+//            .discovery_prefix
+//            .strip_suffix("/")
+//            .unwrap_or(&self.discovery_prefix);
+//        let topic = format!("{prefix}/{component}/{object_id}/config");
+//        let payload = serde_json::ser::to_string(&attributes).unwrap();
+//        let props = PublishProperties {
+//            //payload_format_indicator: Some(1),
+//            message_expiry_interval: Some(ONE_WEEK_SECONDS),
+//            content_type: Some("application/json".to_string()),
+//            ..Default::default()
+//        };
+//        Ok(self
+//            .client
+//            .publish_with_properties(topic, AtLeastOnce, true, payload, props)
+//            .await?)
+//    }
+//
+//    pub async fn publish_data<S: Serialize>(
+//        &self,
+//        topic: &String,
+//        payload: &S,
+//        message_expiry_interval: Option<u32>,
+//    ) -> Result<()> {
+//        let payload = serde_json::ser::to_string(payload).unwrap();
+//        let props = PublishProperties {
+//            message_expiry_interval,
+//            content_type: Some("application/json".to_string()),
+//            ..Default::default()
+//        };
+//        Ok(self
+//            .client
+//            .publish_with_properties(topic, AtLeastOnce, true, payload, props)
+//            .await?)
+//    }
+//}
 
 #[derive(Clone)]
 pub enum Entity {
@@ -162,7 +156,7 @@ impl Entity {
         }
     }
 
-    pub fn get_attributes(&self) -> Result<Value> {
+    pub fn get_attributes(&self) -> Result<Value, Error> {
         let attributes = match self {
             Entity::AlarmControlPanel(alarm_control_panel) => {
                 serde_json::to_value(alarm_control_panel)?
